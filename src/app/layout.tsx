b4d1e5c81d19648835eaf863fd3a6ea96c0e8b92
William Montague <wmontagu@andrew.cmu.edu>
2025-04-10 01:17:28 -0700

    made favicon public

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "4CFC - For College Students, From College Students",
   description: "Helping high schoolers navigate the college application process",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
